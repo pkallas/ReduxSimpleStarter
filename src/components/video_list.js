166427022008880b4168a4b43e898af0756100f3
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-10-24 10:53:10 -0700

    Fixed bug

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -5,7 +5,7 @@ const VideoList = (props) => {
   const videoItems = props.videos.map(video => {
     return (
       <VideoListItem
-        onVideoSelect={props.selectedVideo}
+        onVideoSelect={props.onVideoSelect}
         key={video.etag}
         video={video} />
     );
